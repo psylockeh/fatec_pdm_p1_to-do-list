d973409762c56fa89c97d6e35adb55d5215dcf12
Debora Nogueira <deboran.silva@hotmail.com>
2024-10-08 19:02:28 -0300

    feat(p1-pdmn): cria componente principal e faz hello

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import TarefaEntrada from './TarefaEntrada';
 
 function App() {
+  const [tarefas, setTarefas] = useState([]);
+
+  const adicionarTarefa = (descricao) => {
+    setTarefas([...tarefas, descricao]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <TarefaEntrada adicionarTarefa={adicionarTarefa} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
